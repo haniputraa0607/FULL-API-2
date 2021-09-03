bcc7eab6a3c3c68276359d94a5fad7c5155775f0
tpid-hanif <hanif@technopartner.co.id>
2021-09-03 15:40:09 +0700

    fixing file migration partners and locations table

--- Modules/BusinessDevelopment/Database/Migrations/2021_09_03_140937_create_partners_table.php
+++ Modules/BusinessDevelopment/Database/Migrations/2021_09_03_140937_create_partners_table.php
@@ -14,9 +14,21 @@ class CreatePartnersTable extends Migration
     public function up()
     {
         Schema::create('partners', function (Blueprint $table) {
-            $table->bigIncrements('id');
-
+            $table->increments('id_user_franchise');
+            $table->string('name');
+            $table->string('phone')->nullable();
+            $table->string('email');
+            $table->string('address')->nullable();
+            $table->enum('ownership_status', ['Central','Partner'])->default('Central');
+            $table->enum('cooperation_scheme', ['Profit Sharing','Management Fee'])->default('Profit Sharing');
+            $table->bigInteger('id_bank_account')->unsigned()->index('fk_partner_bank_account');
+            $table->enum('status', ['Active','Inactive','Candidate'])->default('Candidate');
+            $table->dateTime('start_date')->nullable();
+            $table->dateTime('end_date')->nullable();
+            $table->string('password')->nullable();
+            $table->boolean('first_update_password')->default(1);
             $table->timestamps();
+            $table->foreign('id_bank_account', 'fk_partner_bank_account')->references('id_bank_account')->on('bank_accounts')->onDelete('restrict');
         });
     }
 

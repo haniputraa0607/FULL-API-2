021766f393d867657456f8842bf43af4149db010
tpid-hanif <hanif@technopartner.co.id>
2021-09-06 08:31:08 +0700

    updated route business dev

--- Modules/BusinessDevelopment/Routes/api.php
+++ Modules/BusinessDevelopment/Routes/api.php
@@ -16,15 +16,16 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/businessdevelopment', function (Request $request) {
     return $request->user();
 });
-Route::group(['prefix' => 'partners/locations'], function() {
-    Route::any('/', ['uses' => 'ApiLocationsController@index']);
-    Route::post('/delete', ['uses' => 'ApiLocationsController@destroy']);
-    Route::post('/edit', ['uses' => 'ApiLocationsController@edit']);
-    Route::post('/update', ['uses' => 'ApiLocationsController@update']);
-});
+
 Route::group(['prefix' => 'partners'], function() {
     Route::any('/', ['uses' => 'ApiPartnersController@index']);
     Route::post('/delete', ['uses' => 'ApiPartnersController@destroy']);
     Route::post('/edit', ['uses' => 'ApiPartnersController@edit']);
     Route::post('/update', ['uses' => 'ApiPartnersController@update']);
+    Route::group(['prefix' => '/locations'], function() {
+        Route::any('/', ['uses' => 'ApiLocationsController@index']);
+        Route::post('/delete', ['uses' => 'ApiLocationsController@destroy']);
+        Route::post('/edit', ['uses' => 'ApiLocationsController@edit']);
+        Route::post('/update', ['uses' => 'ApiLocationsController@update']);
+    });
 });

42f9a79660ffc3c0db8f9fac93cd82369a2aa9fc
tpid-damar <damar@technopartner.co.id>
2021-09-14 10:50:56 +0700

    [Added] add migrations and models hairstylist schedule

---
+++ Modules/Recruitment/Entities/HairstylistSchedule.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ * Date: Tue, 14 Sep 2021 10:44:38 +0700.
+ */
+
+namespace Modules\Recruitment\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class HairstylistSchedule
+ * 
+ * @property int $id_hairstylist_schedule
+ * @property int $id_user_hair_stylist
+ * @property int $id_outlet
+ * @property int $approve_by
+ * @property \Carbon\Carbon $request_at
+ * @property \Carbon\Carbon $approve_at
+ * @property \Carbon\Carbon $reject_at
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ *
+ * @package Modules\Recruitment\Entities
+ */
+class HairstylistSchedule extends Model
+{
+	protected $primaryKey = 'id_hairstylist_schedule';
+
+	protected $casts = [
+		'id_user_hair_stylist' => 'int',
+		'id_outlet' => 'int',
+		'approve_by' => 'int'
+	];
+
+	protected $dates = [
+		'request_at',
+		'approve_at',
+		'reject_at'
+	];
+
+	protected $fillable = [
+		'id_user_hair_stylist',
+		'id_outlet',
+		'approve_by',
+		'request_at',
+		'approve_at',
+		'reject_at'
+	];
+}
---
+++ Modules/Recruitment/Entities/HairstylistScheduleDate.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ * Date: Tue, 14 Sep 2021 10:44:11 +0700.
+ */
+
+namespace Modules\Recruitment\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class HairstylistScheduleDate
+ * 
+ * @property int $id_hairstylist_schedule_date
+ * @property int $id_hairstylist_schedule
+ * @property \Carbon\Carbon $date
+ * @property string $shift
+ * @property string $request_by
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ *
+ * @package Modules\Recruitment\Entities
+ */
+class HairstylistScheduleDate extends Model
+{
+	protected $primaryKey = 'id_hairstylist_schedule_date';
+
+	protected $casts = [
+		'id_hairstylist_schedule' => 'int'
+	];
+
+	protected $dates = [
+		'date'
+	];
+
+	protected $fillable = [
+		'id_hairstylist_schedule',
+		'date',
+		'shift',
+		'request_by'
+	];
+}

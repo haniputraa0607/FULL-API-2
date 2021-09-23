8cdcf723543a42d9200258bef78cb0d785b0fe6d
tpid-hanif <hanif@technopartner.co.id>
2021-09-23 14:57:48 +0700

    added autocrm approved request update data partner

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -7,27 +7,27 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        $this->call(TextReplacesTableSeeder::class);
-        $this->call(ProvincesTableSeeder::class);
-        $this->call(CitiesTableSeeder::class);
+        // $this->call(TextReplacesTableSeeder::class);
+        // $this->call(ProvincesTableSeeder::class);
+        // $this->call(CitiesTableSeeder::class);
         $this->call(AutocrmsTableSeeder::class);
-        $this->call(CourierTableSeeder::class);
-        $this->call(FeaturesTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
-        $this->call(SettingsTableSeeder::class);
-        $this->call(ConfigsTableSeeder::class);
-        $this->call(OutletsTableSeeder::class);
-        $this->call(ProductCategoriesTableSeeder::class);
-        $this->call(ProductsTableSeeder::class);
-        $this->call(ProductPricesTableSeeder::class);
-        $this->call(UserAddressesTableSeeder::class);
-        $this->call(ManualPaymentsTableSeeder::class);
-        $this->call(ManualPaymentMethodsTableSeeder::class);
+        // $this->call(CourierTableSeeder::class);
+        // $this->call(FeaturesTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
+        // $this->call(SettingsTableSeeder::class);
+        // $this->call(ConfigsTableSeeder::class);
+        // $this->call(OutletsTableSeeder::class);
+        // $this->call(ProductCategoriesTableSeeder::class);
+        // $this->call(ProductsTableSeeder::class);
+        // $this->call(ProductPricesTableSeeder::class);
+        // $this->call(UserAddressesTableSeeder::class);
+        // $this->call(ManualPaymentsTableSeeder::class);
+        // $this->call(ManualPaymentMethodsTableSeeder::class);
 
-        $this->call(AutocrmsTableAddClaimDeals::class);
-        $this->call(SettingJobsCelebrateSeeder::class);
-        $this->call(PromoCampaignsTableSeeder::class);
-        $this->call(PromoCampaignReferralsTableSeeder::class);
-        $this->call(BankNameTableSeeder::class);
+        // $this->call(AutocrmsTableAddClaimDeals::class);
+        // $this->call(SettingJobsCelebrateSeeder::class);
+        // $this->call(PromoCampaignsTableSeeder::class);
+        // $this->call(PromoCampaignReferralsTableSeeder::class);
+        // $this->call(BankNameTableSeeder::class);
     }
 }

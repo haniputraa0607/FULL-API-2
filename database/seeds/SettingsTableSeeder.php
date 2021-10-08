c68b6ae148dc8739c2339a1ce2615ecca61c17fd
tpid-damar <damar@technopartner.co.id>
2021-10-08 15:12:33 +0700

    [Added] add privacy policy to setting seeder

--- database/seeds/SettingsTableSeeder.php
+++ database/seeds/SettingsTableSeeder.php
@@ -1073,6 +1073,15 @@ class SettingsTableSeeder extends Seeder
                     'value_text' => NULL,
                     'created_at' => date('Y-m-d H:i:s'),
                     'updated_at' => date('Y-m-d H:i:s'),
+                ),
+            119 =>
+                array(
+                    'id_setting' => 120,
+                    'key' => 'privacypolicy',
+                    'value' => NULL,
+                    'value_text' => '<h1>Privacy Policy</h1>',
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
                 )
         ));
     }

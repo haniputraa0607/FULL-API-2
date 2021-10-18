1eb8ce074575dd889c6c3296431ec315a05d33f2
tpid-damar <damar@technopartner.co.id>
2021-10-18 17:03:23 +0700

    [Added] add hs announcement migration and model

---
+++ Modules/Recruitment/Entities/HairstylistAnnouncementRuleParent.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ * Date: Mon, 18 Oct 2021 16:58:31 +0700.
+ */
+
+namespace Modules\Recruitment\Entities;
+
+use Reliese\Database\Eloquent\Model as Eloquent;
+
+/**
+ * Class HairstylistAnnouncementRuleParent
+ * 
+ * @property int $id_hairstylist_announcement_rule_parent
+ * @property int $id_hairstylist_announcement
+ * @property string $announcement_rule
+ * @property string $announcement_rule_next
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * 
+ * @property \Modules\Recruitment\Entities\HairstylistAnnouncement $hairstylist_announcement
+ * @property \Illuminate\Database\Eloquent\Collection $hairstylist_announcement_rules
+ *
+ * @package Modules\Recruitment\Entities
+ */
+class HairstylistAnnouncementRuleParent extends Eloquent
+{
+	protected $primaryKey = 'id_hairstylist_announcement_rule_parent';
+
+	protected $casts = [
+		'id_hairstylist_announcement' => 'int'
+	];
+
+	protected $fillable = [
+		'id_hairstylist_announcement',
+		'announcement_rule',
+		'announcement_rule_next'
+	];
+
+	public function hairstylist_announcement()
+	{
+		return $this->belongsTo(\Modules\Recruitment\Entities\HairstylistAnnouncement::class, 'id_hairstylist_announcement');
+	}
+
+	public function hairstylist_announcement_rules()
+	{
+		return $this->hasMany(\Modules\Recruitment\Entities\HairstylistAnnouncementRule::class, 'id_hairstylist_announcement_rule_parent');
+	}
+}

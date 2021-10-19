9084ff125bb51477b07215e910c1dc88b20e4753
raka yulian <raka@technopartner.co.id>
2021-10-19 13:13:48 +0700

    update handover (serahterima)

---
+++ Modules/Project/Database/Migrations/2021_10_19_140322_update_default_on_projects_handover_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateDefaultOnProjectsHandoverTable extends Migration
+{
+    public function __construct() {
+        DB::getDoctrineSchemaManager()->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
+    }
+    public function up()
+    {
+        Schema::table('projects_handover', function (Blueprint $table) {
+            $table->string('attachment',255)->nullable()->change();
+            $table->text('note')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::table('projects_handover', function (Blueprint $table) {
+           $table->string('attachment',255)->nullable()->change();
+           $table->text('note')->nullable()->change();
+        });
+    }
+}

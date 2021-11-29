58bc80f444a776aa61e179b0bc44418a6bd28fe9
Toto Prasetyo <toto@technopartner.co.id>
2021-11-29 15:34:40 +0700

    fix xendit

--- Modules/Transaction/Http/Controllers/ApiConfirm.php
+++ Modules/Transaction/Http/Controllers/ApiConfirm.php
@@ -666,6 +666,7 @@ class ApiConfirm extends Controller
                 ],
             ];
         } elseif ($post['payment_type'] == 'Xendit') {
+            $payment_id = $request->payment_id ?? $request->payment_detail;
             $paymentXendit = TransactionPaymentXendit::where('id_transaction', $check['id_transaction'])->first();
             if(!$paymentXendit) {
                 $paymentXendit = new TransactionPaymentXendit([

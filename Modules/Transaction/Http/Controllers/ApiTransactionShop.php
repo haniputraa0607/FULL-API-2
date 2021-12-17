669cd5ffa8ad119527dfacfeaaff78d54615d56b
Dina <dina@technopartner.co.id>
2021-12-17 15:31:01 +0700

    update and add -list shop and detail be -add data file name in all cash flow

--- Modules/Transaction/Http/Controllers/ApiTransactionShop.php
+++ Modules/Transaction/Http/Controllers/ApiTransactionShop.php
@@ -1493,4 +1493,124 @@ class ApiTransactionShop extends Controller
 
     	return $res;
     }
+
+    public function listShop(Request $request)
+    {
+        $list = Transaction::where('transaction_from', 'shop')
+            ->join('transaction_shops','transactions.id_transaction', 'transaction_shops.id_transaction')
+            ->join('users','transactions.id_user','=','users.id')
+            ->join('outlets', 'outlets.id_outlet', 'transactions.id_outlet')
+            ->leftJoin('transaction_products','transactions.id_transaction','=','transaction_products.id_transaction')
+            ->leftJoin('products','products.id_product','=','transaction_products.id_product')
+            ->with('user')
+            ->select(
+                'transaction_shops.*',
+                'products.*',
+                'transaction_products.*',
+                'outlets.*',
+                'users.*',
+                'transactions.*'
+	            )
+            ->groupBy('transactions.id_transaction');
+
+        $countTotal = null;
+
+        if ($request->rule) {
+            $countTotal = $list->getQuery()->getCountForPagination();
+            $this->filterList($list, $request->rule, $request->operator ?: 'and');
+        }
+
+        if (is_array($orders = $request->order)) {
+            $columns = [
+                'id_transaction',
+                'transaction_date',
+                'outlet_code',
+                'transaction_receipt_number',
+                'name',
+                'phone',
+                'transaction_grandtotal',
+                'transaction_payment_status',
+            ];
+
+            foreach ($orders as $column) {
+                if ($colname = ($columns[$column['column']] ?? false)) {
+                    $list->orderBy($colname, $column['dir']);
+                }
+            }
+        }
+        $list->orderBy('transactions.id_transaction', $column['dir'] ?? 'DESC');
+
+        if ($request->page) {
+            $list = $list->paginate($request->length ?: 15);
+            $list->each(function($item) {
+                $item->images = array_map(function($item) {
+                    return config('url.storage_url_api').$item;
+                }, json_decode($item->images) ?? []);
+            });
+            $list = $list->toArray();
+            if (is_null($countTotal)) {
+                $countTotal = $list['total'];
+            }
+            // needed by datatables
+            $list['recordsTotal'] = $countTotal;
+            $list['recordsFiltered'] = $list['total'];
+        } else {
+            $list = $list->get();
+        }
+        return MyHelper::checkGet($list);
+    }
+
+    public function filterList($model, $rule, $operator = 'and')
+    {
+        $new_rule = [];
+        $where    = $operator == 'and' ? 'where' : 'orWhere';
+        foreach ($rule as $var) {
+            $var1 = ['operator' => $var['operator'] ?? '=', 'parameter' => $var['parameter'] ?? null, 'hide' => $var['hide'] ?? false];
+            if ($var1['operator'] == 'like') {
+                $var1['parameter'] = '%' . $var1['parameter'] . '%';
+            }
+            $new_rule[$var['subject']][] = $var1;
+        }
+        $model->where(function($model2) use ($model, $where, $new_rule){
+            $inner = [
+                'transaction_receipt_number',
+                'order_id',
+                'outlet_name',
+                'outlet_code',
+                'transaction_grandtotal',
+                'transaction_payment_status'
+            ];
+            foreach ($inner as $col_name) {
+                if ($rules = $new_rule[$col_name] ?? false) {
+                    foreach ($rules as $rul) {
+                        $model2->$where($col_name, $rul['operator'], $rul['parameter']);
+                    }
+                }
+            }
+
+            $inner = ['name', 'phone', 'email'];
+            foreach ($inner as $col_name) {
+                if ($rules = $new_rule[$col_name] ?? false) {
+                    foreach ($rules as $rul) {
+                        $model2->$where('users.'.$col_name, $rul['operator'], $rul['parameter']);
+                    }
+                }
+            }
+
+            $inner = ['id_outlet'];
+            foreach ($inner as $col_name) {
+                if ($rules = $new_rule[$col_name] ?? false) {
+                    foreach ($rules as $rul) {
+                        $model2->$where('transactions.'.$col_name, $rul['operator'], $rul['parameter']);
+                    }
+                }
+            }
+        });
+
+        if ($rules = $new_rule['transaction_date'] ?? false) {
+            foreach ($rules as $rul) {
+                $model->where(\DB::raw('DATE(transaction_date)'), $rul['operator'], $rul['parameter']);
+            }
+        }
+    }
 }

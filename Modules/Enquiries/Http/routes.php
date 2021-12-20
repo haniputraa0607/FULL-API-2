5971de011914b1f7a3d83e6f151dc37642541b0e
Dina <dina@technopartner.co.id>
2021-12-20 16:47:55 +0700

    api contact cs and setting subject

--- Modules/Enquiries/Http/routes.php
+++ Modules/Enquiries/Http/routes.php
@@ -8,19 +8,27 @@ Route::group(['middleware' => ['auth:api','user_agent','log_activities', 'scopes
     Route::post('reply', ['middleware' => 'feature_control:84', 'uses' =>'ApiEnquiries@reply']);
     Route::post('update', ['middleware' => 'feature_control:84', 'uses' =>'ApiEnquiries@update']);
     Route::post('delete', ['middleware' => 'feature_control:84', 'uses' =>'ApiEnquiries@delete']);
+    Route::any('setting-subject', 'ApiEnquiries@settingSubject');
 
     Route::post('create', 'ApiEnquiries@createV2');
     Route::post('list-subject', 'ApiEnquiries@listEnquirySubject');
+    Route::post('list-category', 'ApiEnquiries@listEnquiryCategory');
 });
 
 Route::group(['middleware' => ['auth:api','user_agent','log_activities', 'scopes:apps'], 'prefix' => 'api/enquiries', 'namespace' => 'Modules\Enquiries\Http\Controllers'], function()
 {
     Route::post('create', 'ApiEnquiries@createV2');
     Route::post('list-subject', 'ApiEnquiries@listEnquirySubject');
+    Route::post('list-category', 'ApiEnquiries@listEnquiryCategory');
+    Route::post('list-transaction', 'ApiEnquiries@listTransaction');
+    Route::post('detail', 'ApiEnquiries@detail');
 });
 
-Route::group(['middleware' => ['auth:api','user_agent','log_activities', 'scopes:mitra-apps'], 'prefix' => 'api/mitra/enquiries', 'namespace' => 'Modules\Enquiries\Http\Controllers'], function()
+Route::group(['middleware' => ['auth:mitra','user_agent','log_activities', 'scopes:mitra-apps'], 'prefix' => 'api/mitra/enquiries', 'namespace' => 'Modules\Enquiries\Http\Controllers'], function()
 {
     Route::post('create', 'ApiEnquiries@createV2');
     Route::post('list-subject', 'ApiEnquiries@listEnquirySubject');
+    Route::post('list-category', 'ApiEnquiries@listEnquiryCategory');
+    Route::get('list-outlet', 'ApiEnquiries@ListOutlet');
+    Route::post('list-transaction', 'ApiEnquiries@listTransaction');
 });

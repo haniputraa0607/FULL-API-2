15e2cd1c34f2fce5db7f47d1ef4de885fb0e0402
raka yulian <89185735+tpid-raka@users.noreply.github.com>
2021-12-21 10:29:12 +0700

    update autocrm project

--- Modules/Project/Http/Controllers/ApiFitOutController.php
+++ Modules/Project/Http/Controllers/ApiFitOutController.php
@@ -126,6 +126,23 @@ class ApiFitOutController extends Controller
             $value['status'] = "Success";
             $value->save();
         }
+         $project = Project::where(array('id_project'=>$request->id_project))->join('partners','partners.id_partner','projects.id_partner')->first();
+            if (\Module::collections()->has('Autocrm')) {
+                        $autocrm = app($this->autocrm)->SendAutoCRM(
+                            'Update Project',
+                            $project->phone,
+                            [
+                                'name' => $project->name,
+                            ], null, null, null, null, null, null, null, 1,
+                        );
+                        // return $autocrm;
+                        if (!$autocrm) {
+                            return response()->json([
+                                'status'    => 'fail',
+                                'messages'  => ['Failed to send']
+                            ]);
+                        }
+                    }
         $project->save();
          return response()->json(['status' => 'success']);
          }

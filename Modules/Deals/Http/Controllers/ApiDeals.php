1972c813c3c6affb5febb641c1f74b07040f7394
Toto Prasetyo <toto@technopartner.co.id>
2022-02-02 10:43:15 +0700

    fix deals size

--- Modules/Deals/Http/Controllers/ApiDeals.php
+++ Modules/Deals/Http/Controllers/ApiDeals.php
@@ -158,7 +158,7 @@ class ApiDeals extends Controller
                 mkdir($promotionPath??$this->saveImage, 0777, true);
             }
 
-            $upload = MyHelper::uploadPhotoStrict($post['deals_image'], ($promotionPath??$this->saveImage), 500, 500);
+            $upload = MyHelper::uploadPhotoStrict($post['deals_image'], ($promotionPath??$this->saveImage), 750, 375);
 
             if (isset($upload['status']) && $upload['status'] == "success") {
                 $data['deals_image'] = $upload['path'];

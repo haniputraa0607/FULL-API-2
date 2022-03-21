9e5597f352b987269a19968155eba086fd365613
raka yulian <89185735+tpid-raka@users.noreply.github.com>
2022-03-21 10:40:42 +0700

    dev update

--- Modules/Franchise/Http/Controllers/ApiReportSalesController.php
+++ Modules/Franchise/Http/Controllers/ApiReportSalesController.php
@@ -23,40 +23,58 @@ class ApiReportSalesController extends Controller
         }
     	$report = Transaction::where('transactions.id_outlet', $request->id_outlet)
     				->join('transaction_outlet_services', 'transaction_outlet_services.id_transaction', 'transactions.id_transaction')
+    				->join('transaction_products', 'transaction_products.id_transaction', 'transactions.id_transaction')
 					->select(DB::raw('
 						# tanggal transaksi
 						Date(transactions.transaction_date) as transaction_date,
 
 						# total transaksi
-						COUNT(CASE WHEN transactions.id_transaction IS NOT NULL THEN 1 ELSE NULL END) AS total_transaction, 
-
-						# subtotal
-						SUM(CASE WHEN transactions.transaction_gross IS NOT NULL AND transaction_outlet_services.reject_at IS NULL THEN transactions.transaction_gross ELSE 0 END) as total_subtotal,
-
+						COUNT(CASE WHEN transactions.id_transaction IS NOT NULL AND transaction_outlet_services.reject_at IS NULL AND transactions.transaction_payment_status = "Completed" AND transactions.reject_at IS NULL THEN 1 ELSE NULL END) AS total_transaction, 
+                                                
+                                                # tax
+						SUM(
+							CASE WHEN transactions.transaction_tax IS NOT NULL AND transaction_outlet_services.reject_at IS NULL AND transactions.transaction_payment_status = "Completed" AND transactions.reject_at IS NULL THEN transactions.transaction_tax
+								ELSE 0 END
+							) as total_tax,
 						# diskon
 						SUM(
-							CASE WHEN transactions.transaction_discount_item IS NOT NULL AND transaction_outlet_services.reject_at IS NULL THEN ABS(transactions.transaction_discount_item) 
+							CASE WHEN transactions.transaction_discount_item IS NOT NULL AND transaction_outlet_services.reject_at IS NULL AND transactions.transaction_payment_status = "Completed"  AND transactions.reject_at IS NULL THEN ABS(transactions.transaction_discount_item) 
 								WHEN transactions.transaction_discount IS NOT NULL AND transaction_outlet_services.reject_at IS NULL THEN ABS(transactions.transaction_discount)
 								ELSE 0 END
 							+ CASE WHEN transactions.transaction_discount_delivery IS NOT NULL AND transaction_outlet_services.reject_at IS NULL THEN ABS(transactions.transaction_discount_delivery) ELSE 0 END
 							+ CASE WHEN transactions.transaction_discount_bill IS NOT NULL AND transaction_outlet_services.reject_at IS NULL THEN ABS(transactions.transaction_discount_bill) ELSE 0 END
 						) as total_discount,
-                                                 #total_subtotal_complete_payment
-                                                COUNT(CASE WHEN transactions.transaction_gross IS NOT NULL AND transaction_outlet_services.reject_at IS NULL AND transactions.transaction_payment_status = "Completed" THEN 1 ELSE 0 END) as total_subtotal_complete_payment,
                                                 
-                                                # total_grandtotal_complete_payment
-						COUNT(CASE WHEN transactions.transaction_grandtotal IS NOT NULL AND transaction_outlet_services.reject_at IS NULL AND transactions.transaction_payment_status = "Completed"  THEN 1 ELSE 0 END) as total_grandtotal_complete_payment,
-
-						# grandtotal
-						SUM(CASE WHEN transactions.transaction_grandtotal IS NOT NULL AND transaction_outlet_services.reject_at IS NULL THEN transactions.transaction_grandtotal ELSE 0 END) as total_grandtotal,
-
-						# payment complete
-						sum(CASE WHEN transactions.transaction_payment_status = "Completed" THEN 1 ELSE NULL END) as total_complete_payment,
-
-                                                #accept
-                                                COUNT(CASE WHEN transactions.id_transaction AND transaction_outlet_services.reject_at IS NULL THEN 1 ELSE NULL END) as total_accept
+                                                #mdr 
+                                                SUM(
+							CASE WHEN transactions.transaction_tax IS NOT NULL AND transactions.reject_at IS NULL AND transaction_outlet_services.reject_at IS NULL AND transactions.transaction_payment_status = "Completed" THEN transactions.mdr
+								ELSE 0 END
+							) as total_mdr,
+                                                        
+                                                #refund all
+                                                SUM( CASE WHEN transactions.reject_at IS NOT AND transaction_outlet_services.reject_at IS NULL AND transactions.transaction_payment_status = "Completed" THEN transactions.transaction_grandtotal
+								ELSE 0 END
+							) as refund_all,
+                                                #refund product
+                                                SUM(
+                                                CASE WHEN transactions.reject_at IS NULL AND transaction_outlet_services.reject_at IS NULL AND transactions.transaction_payment_status = "Completed" AND transaction_products.reject_at IS NULL THEN transaction_products.transaction_variant_subtotal
+								ELSE 0 END
+							) as refund_product,
+                                                # Total
+						SUM(
+							CASE WHEN  transaction_outlet_services.reject_at IS NULL AND transactions.transaction_payment_status = "Completed"  THEN transactions.trasaction_grandtotal
+								ELSE 0 END
+							) as grand_total,
+                                                #net_sales 
+                                                SUM(
+                                                grand_total - (refund_product + refund_all + total_discount + total_tax)
+							) as total_net_sales,
                                                 
-                                               
+                                                #revenue
+                                                SUM(
+							CASE WHEN transactions.transaction_gross IS NOT NULL AND transaction_outlet_services.reject_at IS NULL AND transactions.transaction_payment_status = "Completed" AND transactions.reject_at IS NULL THEN transactions.transaction_gross
+								ELSE 0 END
+							) as total_revenue,
 					'));
 
         if(isset($post['filter_type']) && $post['filter_type'] == 'range_date'){
@@ -92,34 +110,34 @@ class ApiReportSalesController extends Controller
                 "tooltip" => 'Jumlah semua transaksi',
                 "show" => 1
             ],
-            'total_subtotal' => [
+            'total_revenue' => [
                 'title' => 'Total Revenue',
-                'amount' => 'Rp. '.number_format($report['total_subtotal']??0,0,",","."),
-                "tooltip" => 'Total nominal transaksi sebelum dipotong diskon',
+                'amount' => 'Rp. '.number_format($report['total_revenue']??0,0,",","."),
+                "tooltip" => 'Total pendapatan',
                 "show" => 1
             ],
-            'total_grandtotal' => [
+            'total_diskon' => [
                 'title' => 'Total Diskon Given',
-                'amount' => 'Rp. '.number_format($report['total_grandtotal']??0,0,",","."),
-                "tooltip" => 'Total nominal transaksi setelah dipotong diskon dan ditambah pajak',
+                'amount' => 'Rp. '.number_format($report['total_discount']??0,0,",","."),
+                "tooltip" => 'Total diskon yang dikenakan pada transaksi',
                 "show" => 1
             ],
-             'total_complete_payment' => [
+             'total_tax' => [
                 'title' => 'Total Tax Charged',
-                'amount' => number_format($report['total_complete_payment']??0,0,",","."),
-                "tooltip" => 'jumlah transaksi dengan status pembayaran sukses (mengabaikan status reject order)',
+                'amount' =>'Rp. '. number_format($report['total_tax']??0,0,",","."),
+                "tooltip" => 'Total pajak yang dikenakan pada transaksi',
                 "show" => 1
             ],
-            'total_subtotal_complete_payment' => [
+            'total_mdr_paid' => [
                 'title' => 'Total MDR Paid',
-                'amount' => number_format($report['total_subtotal_complete_payment']??0,0,",","."),
-                "tooltip" => 'Total nominal transaksi sebelum dipotong diskon dengan status pembayaran sukses',
+                'amount' => 'Rp. '.number_format($report['total_mdr']??0,0,",","."),
+                "tooltip" => 'Total nominal MDR transaksi',
                 "show" => 1
             ],
-            'total_grandtotal_complete_payment' => [
+            'total_net_sales' => [
                 'title' => 'Total Net Sales',
-                'amount' => number_format($report['total_grandtotal_complete_payment']??0,0,",","."),
-                "tooltip" => 'Total nominal transaksi setelah dipotong diskon dan ditambah pajak dengan status pembayaran sukses',
+                'amount' => 'Rp. '.number_format($report['total_net_sales']??0,0,",","."),
+                "tooltip" => 'Total pendapatan bersih dari trasaksi',
                 "show" => 1
             ],
     	];

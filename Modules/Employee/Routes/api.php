a81cc4e9a037b5d2873733320ec4ce3f63c2960d
tpid-hanif <tpid-hanif@technopartner.co.id>
2022-04-13 17:08:28 +0700

    updated api detail announ employee

--- Modules/Employee/Routes/api.php
+++ Modules/Employee/Routes/api.php
@@ -28,5 +28,6 @@ Route::group([ 'middleware' => ['log_activities', 'auth:api','user_agent', 'scop
     Route::group(['prefix' => 'announcement'], function(){
         Route::any('/', 'ApiEmployeeAnnouncementController@listAnnouncement');
         Route::post('create', 'ApiEmployeeAnnouncementController@createAnnouncement');
+        Route::post('detail', 'ApiEmployeeAnnouncementController@deailAnnouncement');
     });
 });

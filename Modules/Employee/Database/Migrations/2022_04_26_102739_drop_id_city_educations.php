ab4bb46df9d6dc62a716e4cf76e03a2b7825f2cb
raka yulian <89185735+tpid-raka@users.noreply.github.com>
2022-04-26 16:05:55 +0700

    dev

---
+++ Modules/Employee/Database/Migrations/2022_04_26_102739_drop_id_city_educations.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropIdCityEducations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::drop('employee_educations');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::create('employee_educations', function (Blueprint $table) {
+            $table->Increments('id_employee_education');
+            $table->integer('id_user')->unsigned();
+            $table->enum('educational_level',['SD','SMP','SMA',"Diploma",'Sarjana',"Pascasarjana","Doktoral"])->nullable();
+            $table->string('name_school')->nullable();
+            $table->year('year_education')->nullable();
+            $table->string('study_program')->nullable();
+            $table->integer('id_city_school')->unsigned();
+            $table->foreign('id_user', 'fk_user_employee_educations')->references('id')->on('users')->onDelete('restrict');
+            $table->foreign('id_city_school', 'fk_employee_city_school')->references('id_city')->on('cities')->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+}

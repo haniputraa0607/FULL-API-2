30e0639ac7f9cf60d9ed18ecf81e5273acba02c7
raka yulian <89185735+tpid-raka@users.noreply.github.com>
2022-07-18 14:09:03 +0700

    dev

--- Modules/Recruitment/Http/Requests/loan/CancelLoanIcount.php
+++ Modules/Recruitment/Http/Requests/loan/CancelLoanIcount.php
@@ -7,14 +7,15 @@ use Modules\Recruitment\Entities\UserHairStylist;
 use Modules\Recruitment\Entities\HairstylistLoan;
 use Modules\Recruitment\Entities\HairstylistLoanReturn;
 use Modules\Recruitment\Entities\HairstylistSalesPayment;
-
+use Illuminate\Support\Facades\DB;
 class CancelLoanIcount extends FormRequest
 {
     public function rules()
     {
         return [
             'BusinessPartnerID'    => 'required|cek',
-            'SalesInvoiceID'       => 'required|exists:hairstylist_sales_payments,SalesInvoiceID|cek_payment'
+            'SalesInvoiceID'       => 'required|exists:hairstylist_sales_payments,SalesInvoiceID|cek_payment',
+            'type'                 => 'required|in:IMS,IMA',
         ]; 
     }
 
@@ -32,20 +33,22 @@ class CancelLoanIcount extends FormRequest
            return false;
        });
         $validator->addExtension('cek_payment', function ($attribute, $value, $parameters, $validator) {
-           
-           $sales = HairstylistSalesPayment::where('SalesInvoiceID',$value)->first();
-           if(!$sales){
-               return false;
-           }
-           $loan = HairstylistLoan::where('id_hairstylist_sales_payment',$sales->id_hairstylist_sales_payment)->first();
-           if(!$loan){
-               return false;
-           }
-           $return = HairstylistLoanReturn::where(array(
-               'id_hairstylist_loan'=>$loan->id_hairstylist_loan,
-               'status_return'  => "Success"
-           ))->count();
-           if($return == 0){
+           $data = $validator->getData();
+           $sales = HairstylistSalesPayment::where([
+                        'SalesInvoiceID'=>$value,
+                        'hairstylist_sales_payments.type'=>$data['type']
+                   ])
+                   ->join('hairstylist_loans','hairstylist_loans.id_hairstylist_sales_payment','hairstylist_sales_payments.id_hairstylist_sales_payment')
+                   ->join('hairstylist_loan_returns','hairstylist_loan_returns.id_hairstylist_loan','hairstylist_loans.id_hairstylist_loan')
+                   ->select(DB::raw('
+                                    sum(
+                                   CASE WHEN
+                                   hairstylist_loan_returns.status_return = "Success" THEN 1 ELSE 0
+                                   END
+                                    ) as count
+                                '))
+                   ->first();
+           if($sales->count == 0){
                return true;
            }
            return false;
--- Modules/Recruitment/Http/Requests/loan/CreateLoanIcount.php
+++ Modules/Recruitment/Http/Requests/loan/CreateLoanIcount.php
@@ -4,14 +4,14 @@ namespace Modules\Recruitment\Http\Requests\loan;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Modules\Recruitment\Entities\UserHairStylist;
-
+use Modules\Recruitment\Entities\HairstylistSalesPayment;
 class CreateLoanIcount extends FormRequest
 {
     public function rules()
     {
         return [
             'BusinessPartnerID'    => 'required|cek',
-            'SalesInvoiceID'       => 'required|uniques:hairstylist_sales_payments,SalesInvoiceID',
+            'SalesInvoiceID'       => 'required|cek_sales',
             'amount'               => 'required|numeric',
             'type'                 => 'required|in:IMS,IMA',
         ]; 
@@ -30,12 +30,24 @@ class CreateLoanIcount extends FormRequest
            }
            return false;
        });
+        $validator->addExtension('cek_sales', function ($attribute, $value, $parameters, $validator) {
+            $data = $validator->getData();
+            $share = HairstylistSalesPayment::where(array(
+                'SalesInvoiceID'=>$value,
+                'type'=>$data['type']
+                    ))->first();   
+           if(!$share){
+               return true;
+           }
+           return false;
+       });
     }
 
     public function messages()
     {
         return [
             'cek' => 'Business Partner ID not found',
+            'cek_sales' => 'Sales Invoice ID is exists',
         ];
     }
     public function authorize()

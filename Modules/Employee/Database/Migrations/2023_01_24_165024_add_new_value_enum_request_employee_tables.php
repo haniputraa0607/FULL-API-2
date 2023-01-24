005b32601eabf022aa9bfa0e03e33f918b339bdb
tpid-hanif <tpid-hanif@technopartner.co.id>
2023-01-24 16:51:35 +0700

    update api employee request

---
+++ Modules/Employee/Database/Migrations/2023_01_24_165024_add_new_value_enum_request_employee_tables.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNewValueEnumRequestEmployeeTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('ALTER TABLE `request_employees` CHANGE `status` `status` ENUM("Request","Approved", "Rejected", "Done Approved", "Director Approved", "Finished") default "Request" NOT NULL;');
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('ALTER TABLE `request_employees` CHANGE `status` `status` ENUM("Request","Approved", "Rejected", "Done Approved", "Finished") default "Request" NOT NULL;');
+    }
+}

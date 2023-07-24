528060cfb29559e9826ea793c54cbdce1b2385bb
tpid-hanif <tpid-hanif@technopartner.co.id>
2023-07-24 10:00:31 +0700

    try to fix sync icount

--- app/Jobs/SyncIcountChartOfAccount.php
+++ app/Jobs/SyncIcountChartOfAccount.php
@@ -37,40 +37,42 @@ class SyncIcountChartOfAccount implements ShouldQueue
         $icount = new Icount();
         $id_chart = $this->data['id_chart'];
         $data = $icount->ChartOfAccount($this->data['page']);
-        if($data['response']['Status']==0 && $data['response']['Message']=='Success'){
-            if($data['response']['Meta']['Pagination']['CurrentPage']==1){
-                $index = 0;
-            }else{
-                $index = count($id_chart);
-            }
-            foreach ($data['response']['Data'] as $value) {
-                $id_chart[$index] = $value['ChartOfAccountID'];
-                $query = ChartOfAccount::where(array('ChartOfAccountID'=>$value['ChartOfAccountID']))->first();
-                if($query){
-                   $query->ChartOfAccountID = $value['ChartOfAccountID'];
-                   $query->CompanyID        = $value['CompanyID'];
-                   $query->GroupAccountID   = $value['GroupAccountID'];
-                   $query->AccountNo        = $value['AccountNo'];
-                   $query->Description      = $value['Description'];
-                   $query->ParentID         = $value['ParentID'];
-                   $query->IsChildest       = $value['IsChildest'];
-                   $query->IsBank           = $value['Description'];
-                   $query->Type             = $value['Type'];
-                   $query->IsDeleted        = $value['Description'];
-                   $query->save();
+        if(isset($data) && isset($data['response'])){
+            if($data['response']['Status']==0 && $data['response']['Message']=='Success'){
+                if($data['response']['Meta']['Pagination']['CurrentPage']==1){
+                    $index = 0;
                 }else{
-                   $create =  ChartOfAccount::create($value);
+                    $index = count($id_chart);
                 }
-                $index++;
-            }
-            if($data['response']['Meta']['Pagination']['CurrentPage']<$data['response']['Meta']['Pagination']['LastPage']){
-                $new_page = $data['response']['Meta']['Pagination']['CurrentPage'] + 1;
-                SyncIcountChartOfAccount::dispatch(['page'=> $new_page,'id_chart' => $id_chart])->onConnection('syncicountchartofaccount');  
-                Setting::where('key','Sync Chart Icount')->update(['value' => 'process']);
-            }else{
-                ChartOfAccount::whereIn('ChartOfAccountID',$id_chart)->update(['is_actived' => 'true']);
-                ChartOfAccount::whereNotIn('ChartOfAccountID',$id_chart)->update(['is_actived' => 'false']);
-                Setting::where('key','Sync Chart Icount')->update(['value' => 'finished']);
+                foreach ($data['response']['Data'] as $value) {
+                    $id_chart[$index] = $value['ChartOfAccountID'];
+                    $query = ChartOfAccount::where(array('ChartOfAccountID'=>$value['ChartOfAccountID']))->first();
+                    if($query){
+                       $query->ChartOfAccountID = $value['ChartOfAccountID'];
+                       $query->CompanyID        = $value['CompanyID'];
+                       $query->GroupAccountID   = $value['GroupAccountID'];
+                       $query->AccountNo        = $value['AccountNo'];
+                       $query->Description      = $value['Description'];
+                       $query->ParentID         = $value['ParentID'];
+                       $query->IsChildest       = $value['IsChildest'];
+                       $query->IsBank           = $value['Description'];
+                       $query->Type             = $value['Type'];
+                       $query->IsDeleted        = $value['Description'];
+                       $query->save();
+                    }else{
+                       $create =  ChartOfAccount::create($value);
+                    }
+                    $index++;
+                }
+                if($data['response']['Meta']['Pagination']['CurrentPage']<$data['response']['Meta']['Pagination']['LastPage']){
+                    $new_page = $data['response']['Meta']['Pagination']['CurrentPage'] + 1;
+                    SyncIcountChartOfAccount::dispatch(['page'=> $new_page,'id_chart' => $id_chart])->onConnection('syncicountchartofaccount');  
+                    Setting::where('key','Sync Chart Icount')->update(['value' => 'process']);
+                }else{
+                    ChartOfAccount::whereIn('ChartOfAccountID',$id_chart)->update(['is_actived' => 'true']);
+                    ChartOfAccount::whereNotIn('ChartOfAccountID',$id_chart)->update(['is_actived' => 'false']);
+                    Setting::where('key','Sync Chart Icount')->update(['value' => 'finished']);
+                }
             }
         }
     }

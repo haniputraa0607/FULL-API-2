b57013ce815ea8a305e8e517a18d7c75c9e43dce
rakayl <raka@technopartner.co.id>
2023-08-14 09:29:55 +0700

    Generate & Fix Commission HS

---
+++ app/Jobs/CommissionGenerate.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Http\Models\DealsUser;
+use Modules\Report\Entities\ExportQueue;
+use App\Http\Models\Setting;
+use App\Http\Models\User;
+use Modules\Recruitment\Entities\ExportPayrollQueue;
+use Modules\Recruitment\Entities\HairstylistPayrollQueue;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Rap2hpoutre\FastExcel\FastExcel;
+use DB;
+use Storage;
+use Excel;
+use App\Lib\SendMail as Mail;
+use Mailgun;
+use File;
+use Symfony\Component\HttpFoundation\Request;
+use App\Lib\MyHelper;
+use Modules\PortalPartner\Entities\OutletReportJob;
+
+class CommissionGenerate implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+       
+        $datas = app('Modules\Transaction\Http\Controllers\ApiTransactionProductionController')->breakdownCommissionQueue();
+        return true;
+
+    }
+}

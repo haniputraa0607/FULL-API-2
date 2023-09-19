0f5d5b55d23510f68d791e3994c3423fbd5c7178
rakayl <raka@technopartner.co.id>
2023-09-19 17:03:18 +0700

    Update Update.php

--- Modules/Employee/Http/Requests/Reimbursement/Update.php
+++ Modules/Employee/Http/Requests/Reimbursement/Update.php
@@ -6,11 +6,38 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Modules\Employee\Entities\EmployeeReimbursement;
+use Modules\Product\Entities\ProductIcount;
+use App\Http\Models\Outlet;
 use Illuminate\Support\Facades\Auth;
 class Update extends FormRequest
 {
       public function withValidator($validator)
     {
+         $validator->addExtension('cek', function ($attribute, $value, $parameters, $validator) {
+         $post =  Auth::user();
+            $outlet = Outlet::leftjoin('locations','locations.id_location','outlets.id_location')->where('id_outlet',$post["id_outlet"])->select('company_type')->first();
+            if($outlet['company_type']??''=="PT IMA"){
+                $company = 'ima';
+            }else{
+                $company = 'ims';
+            }
+          $survey = $data = ProductIcount::join('employee_reimbursement_product_icounts','employee_reimbursement_product_icounts.id_product_icount','product_icounts.id_product_icount')
+               ->where([
+           'is_buyable'=>'true',
+           'is_sellable'=>'true',
+           'is_deleted'=>'false',
+           'is_suspended'=>'false',
+           'is_actived'=>'true',
+           'company_type'=>$company
+       ])->select([
+           'product_icounts.id_product_icount',
+           'product_icounts.name',
+           'product_icounts.code'
+       ])->first();
+         if($survey){
+             return true;
+         } return false;
+        }); 
         $validator->addExtension('reimbursement', function ($attribute, $value, $parameters, $validator) {
          $survey = EmployeeReimbursement::where(array('id_employee_reimbursement'=>$value,'status'=>"Pending"))->count();
          if($survey != 0){

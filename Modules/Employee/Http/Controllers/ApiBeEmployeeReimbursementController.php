60b7794adf381fe78f6d21d46d5978a83ae4d523
rakayl <raka@technopartner.co.id>
2023-09-21 13:36:55 +0700

    dev

--- Modules/Employee/Http/Controllers/ApiBeEmployeeReimbursementController.php
+++ Modules/Employee/Http/Controllers/ApiBeEmployeeReimbursementController.php
@@ -420,5 +420,49 @@ class ApiBeEmployeeReimbursementController extends Controller
        }
        return MyHelper::checkGet($data);
    }
-   
+   public function approved_apps($request) {
+       $post = $request->all();
+       $post['date_validation'] = date('Y-m-d H:i:s');
+       $post['id_user_approved'] =  $post['id_user_approved'] ?? Auth::user()->id;
+       $reimbursement = EmployeeReimbursement::where(array('id_employee_reimbursement'=>$request->id_employee_reimbursement))->update($post);
+       $reimbursement = EmployeeReimbursement::join('users','users.id','employee_reimbursements.id_user')->where(array('id_employee_reimbursement'=>$request->id_employee_reimbursement))->first();
+       if($post['status'] == "Approved"){
+            $data_send = [
+                    "reimbursement" => EmployeeReimbursement::where(array('id_employee_reimbursement'=>$request->id_employee_reimbursement))->first(),
+                    "employee" => Employee::where('id_user',$reimbursement['id_user'])->first(),
+                    "item"=> ProductIcount::where('id_product_icount',$reimbursement['id_product_icount'])->first(),
+                    "outlet" => Outlet::where('id_outlet',$reimbursement["id_outlet"])->first(),
+                    "location" => Outlet::leftjoin('locations','locations.id_location','outlets.id_location')->where('id_outlet',$reimbursement["id_outlet"])->first(),
+                ];
+              $initBranch = Icount::EmployeeReimbursement($data_send, $data_send['location']['company_type']??null);
+               if($initBranch['response']['Status']=='1' && $initBranch['response']['Message']=='success'){
+                   $initBranch = $initBranch['response']['Data'][0];
+                   $update = EmployeeReimbursement::where(array('id_employee_reimbursement'=>$request->id_employee_reimbursement))->update([
+                       'id_purchase_invoice'=>$initBranch['PurchaseInvoiceID'],
+                       'value_detail'=> json_encode($initBranch)
+                   ]);
+               }
+               if (\Module::collections()->has('Autocrm')) {
+                   $getData = EmployeeReimbursement::join('users','users.id','employee_reimbursements.id_user')
+                        ->where('id_employee_reimbursement',$post['id_employee_reimbursement'])
+                        ->select('users.phone')
+                        ->first();
+                        $autocrm = app($this->autocrm)->SendAutoCRM(
+                            'Employee Reimbursement Approved',
+                            $getData->phone,
+                            [
+                                'document_type'=> $post['status'],
+                            ], null, null, null, null, null, null, null, null,
+                        );
+                        // return $autocrm;
+                        if (!$autocrm) {
+                            return response()->json([
+                                'status'    => 'fail',
+                                'messages'  => ['Failed to send']
+                            ]);
+                        }
+                    }
+       }
+       return MyHelper::checkGet($reimbursement);
+   }
 }

500336cc6575cc3acb27e34bcdac93df0095082c
rakayl <raka@technopartner.co.id>
2023-11-03 13:04:56 +0700

    dev update clearlog

--- Modules/Setting/Http/Controllers/ApiClearLog.php
+++ Modules/Setting/Http/Controllers/ApiClearLog.php
@@ -14,14 +14,14 @@ class ApiClearLog extends Controller
         return 'true';
     }
     public function clearLog() {
-        $date = Carbon::now()->subDays(30);
-        $dates = Carbon::now()->subDays(90);
-        $failed = \App\Http\Models\LogActivitiesApps::where('created_at', '<=', $date)->first();
-        $failed = \App\Http\Models\LogActivitiesBE::where('created_at', '<=', $date)->delete();
-        $failed = \App\Http\Models\LogActivitiesMitraApp::where('created_at', '<=', $date)->delete();
-        $failed = \App\Http\Models\LogActivitiesOutletApps::where('created_at', '<=', $date)->delete();
-        $failed = \Modules\Xendit\Entities\LogXendit::where('created_at', '<=', $dates)->delete();
-        $failed = \App\Http\Models\FailedJob::where('failed_at', '<=', $date)->first();
+//        $date = Carbon::now()->subDays(30);
+//        $dates = Carbon::now()->subDays(90);
+//        $failed = \App\Http\Models\LogActivitiesApps::where('created_at', '<=', $date)->first();
+//        $failed = \App\Http\Models\LogActivitiesBE::where('created_at', '<=', $date)->delete();
+//        $failed = \App\Http\Models\LogActivitiesMitraApp::where('created_at', '<=', $date)->delete();
+//        $failed = \App\Http\Models\LogActivitiesOutletApps::where('created_at', '<=', $date)->delete();
+//        $failed = \Modules\Xendit\Entities\LogXendit::where('created_at', '<=', $dates)->delete();
+//        $failed = \App\Http\Models\FailedJob::where('failed_at', '<=', $date)->first();
         return true;
     }
 }
